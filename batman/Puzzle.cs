efe5b3f1e76c193bacf1cc3a88afe87542db49f1
Austin Short <austinshort1@gmail.com>
2018-01-16 18:41:56 -0700

    Add files via upload

---
+++ batman/Puzzle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace batman
+{
+    internal class Puzzle
+    {
+        public Card[] spots = new Card[9];
+        public int active_index
+        {
+            get;set;
+        }
+        public void Print()
+        {
+            for (int i =0; i< 9; i++)
+            {
+                if (spots[i] == null)
+                {
+                    Console.Write("| empty |");
+                }
+                else
+                {
+                    spots[i].Print();
+                }
+                if ((i + 1)%3 ==0)
+                {
+                    Console.WriteLine("\n");
+                }
+            }
+        }
+    }
+}
